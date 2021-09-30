992614036fe89d61149505725b12d3002a64aa3c
denis <denisbueltan@gmail.com>
2021-09-30 19:53:53 -0300

    listAdresses is paramether too

--- lib/data_connection_checker.dart
+++ lib/data_connection_checker.dart
@@ -20,6 +20,9 @@ class DataConnectionChecker {
   /// here:
   /// - https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
   /// - https://www.google.com/search?q=dns+server+port
+  List<AddressCheckOptions> ListAdress;
+
+  DataConnectionChecker({this.ListAdress});
   static const int DEFAULT_PORT = 53;
 
   /// Default timeout is 10 seconds.
@@ -87,6 +90,10 @@ class DataConnectionChecker {
   ///
   /// See [AddressCheckOptions] for more info.
   List<AddressCheckOptions> addresses = DEFAULT_ADDRESSES;
+
+  if(listAdress != null) {
+    addresses = listAdress;
+  }
 
   /// This is a singleton that can be accessed like a regular constructor
   /// i.e. DataConnectionChecker() always returns the same instance.
